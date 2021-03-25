dad27785d701f87341e4ceda441bb98eb3f05573
Néstor G. Trillo <nospam@avaniu.com>
2021-03-25 10:07:24 -1000

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Nestor Trillo on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
